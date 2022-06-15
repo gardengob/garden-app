8e796c47936e1a3c3fc2e03a0811094de9052255
Lou <batierlou@gmail.com>
2022-06-15 09:00:57 +0200

    starting

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -2,8 +2,9 @@ import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import RoutingCameraService from '../services/events/RoutingCameraService'
 import { Component3dName } from '../webGL/webGLArchitecture/Types/Component3dNameType'
+import css from './notifications.module.scss'
 
-export default function MailBox() {
+export default function Notifications() {
   const CAMERA_POSITION: Component3dName = 'mail_box'
 
   const router = useRouter()
@@ -14,5 +15,12 @@ export default function MailBox() {
 
   useEffect(() => {
     RoutingCameraService.goTo(CAMERA_POSITION)
+    localStorage.setItem('lockScroll', 'true')
   }, [])
+
+  return (
+    <div className={css.root}>
+      Notifications
+    </div>
+  )
 }
