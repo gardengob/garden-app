f57e9980e2e000509de2be28bb07be44d3453f9e
Lou <batierlou@gmail.com>
2022-06-22 01:41:39 +0200

    loader number

--- src/components/loadingOverlay/LoadingOverlay.tsx
+++ src/components/loadingOverlay/LoadingOverlay.tsx
@@ -1,11 +1,14 @@
 import css from './LoadingOverlay.module.scss'
 import Lottie from 'lottie-web'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import loadingJson from '../../lotties/anim-loading.json'
+import LoadingService from '../../services/events/LoadingService'
 
 export default function LoadingOverlay() {
   const loadingRefEl = useRef(null)
   const loadingRef = useRef(null)
+
+  const [loadingNumber, setloadingNumber] = useState(0)
 
   useEffect(() => {
     // SETUP LOTTIE.S THAT ARE USED IN COMPONENT
@@ -16,11 +19,16 @@ export default function LoadingOverlay() {
       autoplay: true,
       animationData: loadingJson,
     })
+
+    LoadingService.loadingNumberSignal.on((number) => {
+      setloadingNumber(number)
+    })
   }, [])
 
   return (
     <div className={css.root}>
       <div className={css.lottie} ref={loadingRefEl}></div>
+      <div className={css.number}>{loadingNumber}%</div>
     </div>
   )
 }
