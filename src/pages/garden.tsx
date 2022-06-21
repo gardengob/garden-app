8281b24092a5192449faf5df2a186250ea983e4d
Lou <batierlou@gmail.com>
2022-06-21 09:44:59 +0200

    change entrance img

--- src/pages/garden.tsx
+++ src/pages/garden.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import RoutingCameraService from '../services/events/RoutingCameraService'
@@ -28,10 +29,11 @@ export default function Garden() {
     <>
       {!intro && router.query.withIntro && (
         <>
-          <img
+          <Image
             className={css.decors}
             src="/images/ui/decors_plantes-big.png"
             alt=""
+            layout='fill'
           />
           <div
             className={css.garden}
